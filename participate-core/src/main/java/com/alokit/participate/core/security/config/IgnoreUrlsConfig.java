f29293a811c201fc2d05b33180513974c5cb3895
Alokit Jindal <iamalokit@gmail.com>
2020-12-22 17:01:13 +0530

    Added the parameter from which the bypassed urls should be taken

--- participate-core/src/main/java/com/alokit/participate/core/security/config/IgnoreUrlsConfig.java
+++ participate-core/src/main/java/com/alokit/participate/core/security/config/IgnoreUrlsConfig.java
@@ -3,6 +3,9 @@ package com.alokit.participate.core.security.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "secure.ignored")
 public class IgnoreUrlsConfig {
 	private List<String> urls = new ArrayList<String>();
 
